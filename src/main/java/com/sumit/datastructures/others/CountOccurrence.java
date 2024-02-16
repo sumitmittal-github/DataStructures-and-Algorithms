c762edd759f3487274faa2273a1141f5bf77f1a3
Sumit Mittal <sumitmittal.adonis@gmail.com>
2024-02-15 19:26:48 -0500

    added Count digit occurrence in program

---
+++ src/main/java/com/sumit/datastructures/others/CountOccurrence.java
@@ -0,0 +1,35 @@
+package com.sumit.datastructures.others;
+
+import java.util.Scanner;
+
+public class CountOccurrence {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Please enter the number and the integer to find in that number : ");
+        long number = scanner.nextLong();
+        long findNum = scanner.nextLong();
+
+        while(number >= 0) {
+            System.out.println(CountOccurrenceInNumber(number, findNum));
+            System.out.print("Please enter the number and the integer to find in that number : ");
+            number = scanner.nextLong();
+            findNum = scanner.nextLong();
+        }
+
+        System.out.print("Algorithm finished !!");
+    }
+
+
+    private static int CountOccurrenceInNumber(Long number, Long findNum) {
+        int count = 0;
+        for(String eachNum : number.toString().split("")) {
+            if(eachNum.equals(findNum.toString()))
+                count++;
+        }
+        return count;
+    }
+
+
+}
