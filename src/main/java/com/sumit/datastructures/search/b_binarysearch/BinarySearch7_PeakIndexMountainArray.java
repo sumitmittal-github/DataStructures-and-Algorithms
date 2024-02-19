5943d66911253113563956bbc7f2ebd0adad071f
Sumit Mittal <sumitmittal.adonis@gmail.com>
2024-02-19 14:18:46 -0500

    Binary search : Leet code problem : Peak Index Mountain Array

---
+++ src/main/java/com/sumit/datastructures/search/b_binarysearch/BinarySearch7_PeakIndexMountainArray.java
@@ -0,0 +1,37 @@
+package com.sumit.datastructures.search.b_binarysearch;
+
+import java.util.Arrays;
+
+public class BinarySearch7_PeakIndexMountainArray {
+
+    // https://leetcode.com/problems/peak-index-in-a-mountain-array/description/
+
+    public static void main(String[] args) {
+                     // Index : 0  1  2  3  4  5  6  7  8   9  10
+        int nums1[] = new int[]{0, 1, 2, 3, 4, 5, 6, 7, 10, 5, 2};
+        System.out.println("Mountain : " + Arrays.toString(nums1) + " => Peak index : " + peakIndexInMountainArray(nums1));
+
+                     // Index : 0  1  2  3  4
+        int nums2[] = new int[]{3, 5, 3, 2, 0};
+        System.out.println("Mountain : " + Arrays.toString(nums2) + " => Peak index : " + peakIndexInMountainArray(nums2));
+
+                     // Index : 0  1  2  3  4  5  6
+        int nums3[] = new int[]{0, 1, 2, 3, 4, 5, 4};
+        System.out.println("Mountain : " + Arrays.toString(nums3) + " => Peak index : " + peakIndexInMountainArray(nums3));
+    }
+
+    private static int peakIndexInMountainArray(int[] arr) {
+        int start = 0, end = arr.length-1;
+
+        while(start < end) {
+            int mid = start + (end-start)/2;
+
+            if(arr[mid] > arr[mid+1])
+                end = mid;
+            else
+                start = mid+1;
+        }
+        return start;
+    }
+
+}
