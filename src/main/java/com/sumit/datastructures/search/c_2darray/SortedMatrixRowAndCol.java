d4f00c774fb477468621b5efcff4f9c35010aaa5
Sumit Mittal <sumitmittal.adonis@gmail.com>
2024-02-23 19:08:15 -0500

    2D Array : Sorted by rows and columns

---
+++ src/main/java/com/sumit/datastructures/search/c_2darray/SortedMatrixRowAndCol.java
@@ -0,0 +1,46 @@
+package com.sumit.datastructures.search.c_2darray;
+
+import org.springframework.http.server.DelegatingServerHttpResponse;
+
+import java.util.Arrays;
+
+public class SortedMatrixRowAndCol {
+
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {10, 20, 30, 40},
+                {15, 25, 35, 45},
+                {28, 29, 37, 49},
+                {33, 34, 55, 60}
+        };
+
+        int target1 = 37;
+        System.out.println("target : "+ target1 +" => indexes : " + Arrays.toString(search(matrix, target1)));
+
+        int target2 = 10;
+        System.out.println("target : "+ target2 +" => indexes : " + Arrays.toString(search(matrix, target2)));
+
+        int target3 = 100;
+        System.out.println("target : "+ target3 +" => indexes : " + Arrays.toString(search(matrix, target3)));
+
+        int target4 = -100;
+        System.out.println("target : "+ target4 +" => indexes : " + Arrays.toString(search(matrix, target4)));
+    }
+
+    private static int[] search(int[][] matrix, int target) {
+        int row=0;
+        int col = matrix.length-1;
+
+        while(row <= matrix.length-1 && col >= 0){
+            int element = matrix[row][col];
+            if(target == element)
+                return new int[]{row, col};
+            else if(target > element)
+                row++;
+            else
+                col--;
+        }
+        return new int[]{-1, -1};
+    }
+
+}
