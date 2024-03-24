5cc92db0b91781644303ee39b37e3271e8b5bd88
Sumit Mittal <sumitmittal.adonis@gmail.com>
2024-03-24 13:49:17 -0400

    code enhanced

--- src/main/java/com/sumit/datastructures/mixprograms/PrimeNumber.java
+++ src/main/java/com/sumit/datastructures/mixprograms/PrimeNumber.java
@@ -1,5 +1,8 @@
 package com.sumit.datastructures.mixprograms;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PrimeNumber {
 
     public static void main(String[] args) {
@@ -9,21 +12,36 @@ public class PrimeNumber {
         System.out.println("17 is prime : "+ isPrime(17));
         System.out.println("123 is prime : "+ isPrime(123));
         System.out.println("1117111111 is prime : "+ isPrime(1117111111));
+
+        int n=36;
+        System.out.println("All prime numbers till " + n + " are : " + findAllPrimeNumbersTillN(n));
     }
 
 
+    // Complexity = sqrt(n)
     private static boolean isPrime(int inputNumber){
-        boolean isPrime=true;
+        if(inputNumber <= 1)
+            return false;
+
         int num=2;
         while(num*num <= inputNumber) {
-            if(inputNumber % num == 0) {
-                isPrime=false;
-                break;
-            } else {
-                ++num;
-            }
+            if(inputNumber % num == 0)
+                return false;
+            ++num;
         }
-        return isPrime;
+        return true;
+    }
+
+
+
+    // bad approach because complexity will be n*sqrt(n)
+    private static List<Integer> findAllPrimeNumbersTillN(int n){
+        List<Integer> primeNums = new ArrayList<>();
+        for(int i=1; i<=n ; i++) {
+            if(isPrime(i))
+                primeNums.add(i);
+        }
+        return primeNums;
     }
 
 
