7671409d62c30edc21e1064f46004900feb3bc33
Sumit Mittal <sumitmittal.adonis@gmail.com>
2024-03-24 18:15:14 -0400

    Factors of a number with general approach

---
+++ src/main/java/com/sumit/datastructures/factors/Factor_1_GeneralApproach.java
@@ -0,0 +1,17 @@
+package com.sumit.datastructures.factors;
+
+public class Factor_1_GeneralApproach {
+
+    public static void main(String[] args) {
+        System.out.println("Factors of num 20 are : ");
+        factors(20);
+    }
+
+    private static void factors(int num) {
+        for (int i = 1; i <= num; i++) {
+            if(num % i == 0)
+                System.out.print(i + " ");
+        }
+    }
+
+}
