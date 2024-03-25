c4ea5a855c3f4b18567b574f47a6ce1f716b9936
Sumit Mittal <sumitmittal.adonis@gmail.com>
2024-03-25 19:06:58 -0400

    Recursion : Sum of a numbers from n to 1

---
+++ src/main/java/com/sumit/datastructures/recursion/Recursion4_SumOfNto1.java
@@ -0,0 +1,18 @@
+package com.sumit.datastructures.recursion;
+
+public class Recursion4_SumOfNto1 {
+
+    public static void main(String[] args) {
+        System.out.println(sum(5));
+        System.out.println(sum(10));
+    }
+
+    private static int sum(int n){
+        if (n <= 1)
+          return 1;
+
+        return n + sum(n-1);
+    }
+
+
+}
