420ee7faa94e97271b6902b349fb8b06e05a118a
Sumit Mittal <sumitmittal.adonis@gmail.com>
2024-03-26 19:35:07 -0400

    Recursion : Palindrome

--- src/main/java/com/sumit/datastructures/mixprograms/Palindrome.java
+++ src/main/java/com/sumit/datastructures/mixprograms/Palindrome.java
@@ -3,7 +3,8 @@ package com.sumit.datastructures.mixprograms;
 public class Palindrome {
 
     public static void main(String[] args) {
-/*
+
+        // Way-1
         System.out.println("null : " + isPalindrome(null));
         System.out.println(" : " + isPalindrome(""));
         System.out.println("a : " + isPalindrome("a"));
@@ -15,16 +16,26 @@ public class Palindrome {
         System.out.println("abca : " + isPalindrome("abca"));
         System.out.println("abcBA : " + isPalindrome("abcBA"));
         System.out.println("abcab : " + isPalindrome("abcab"));
-*/
 
 
+        // Way-2
         System.out.println(isPalindromeByRecursion(121));
         System.out.println(isPalindromeByRecursion(1221));
         System.out.println(isPalindromeByRecursion(123421));
         System.out.println(isPalindromeByRecursion(11));
         System.out.println(isPalindromeByRecursion(1));
+
+
+        // Way-3
+        System.out.println(isPalindromeByRecursion2(121));
+        System.out.println(isPalindromeByRecursion2(1221));
+        System.out.println(isPalindromeByRecursion2(123421));
+        System.out.println(isPalindromeByRecursion2(11));
+        System.out.println(isPalindromeByRecursion2(1));
     }
 
+
+    // Way-1 : general indexes approach
     private static boolean isPalindrome(String str) {
         if(str == null || str.length() == 0)
             return false;
@@ -48,6 +59,8 @@ public class Palindrome {
         return true;
     }
 
+
+    // Way-2 : by using helper function and checking =>   num == rev(num)
     private static boolean isPalindromeByRecursion(int num) {
         int revNum = reverseNumByRecursion(num);
         return revNum == num;
@@ -59,6 +72,24 @@ public class Palindrome {
 
         int numOfDigitsInNum = (int)Math.log10(num)+1;
         return ((num%10) * (int)Math.pow(10, numOfDigitsInNum-1)) + reverseNumByRecursion(num/10);
+    }
+
+
+    // Way-3 : by using helper function and checking =>   indexes from start and end of the number in string
+    private static boolean isPalindromeByRecursion2(int num) {
+        String numStr = String.valueOf(num);
+        int startIndex = 0;
+        int endIndex = numStr.length()-1;
+        return isPalindromeByRecursion2Helper(numStr, startIndex, endIndex);
+    }
+
+    private static boolean isPalindromeByRecursion2Helper(String numStr, int startIndex, int endIndex){
+        if(startIndex >= endIndex)
+            return true;
+        if(numStr.charAt(startIndex) != numStr.charAt(endIndex))
+            return false;
+
+        return isPalindromeByRecursion2Helper(numStr, ++startIndex, --endIndex);
     }
 
 }
--- src/main/java/com/sumit/datastructures/recursion/Recursion8_Palindrome.java
+++ src/main/java/com/sumit/datastructures/recursion/Recursion8_Palindrome.java
@@ -4,14 +4,23 @@ public class Recursion8_Palindrome {
 
     public static void main(String[] args) {
         System.out.println(isPalindromeByRecursion(121));
+        System.out.println(isPalindromeByRecursion2(121));
+
         System.out.println(isPalindromeByRecursion(1221));
+        System.out.println(isPalindromeByRecursion2(1221));
+
         System.out.println(isPalindromeByRecursion(123421));
+        System.out.println(isPalindromeByRecursion2(123421));
+
         System.out.println(isPalindromeByRecursion(11));
+        System.out.println(isPalindromeByRecursion2(11));
+
         System.out.println(isPalindromeByRecursion(1));
+        System.out.println(isPalindromeByRecursion2(1));
     }
 
 
-    // Way-1 : by using helper function
+    // Way-1 : by using helper function and checking =>   num == rev(num)
     private static boolean isPalindromeByRecursion(int num) {
         int revNum = reverseNumByRecursion(num);
         return revNum == num;
@@ -26,6 +35,22 @@ public class Recursion8_Palindrome {
     }
 
 
+    // Way-2 : by using helper function and checking =>   indexes from start and end of the number in string
+    private static boolean isPalindromeByRecursion2(int num) {
+        String numStr = String.valueOf(num);
+        int startIndex = 0;
+        int endIndex = numStr.length()-1;
+        return isPalindromeByRecursion2Helper(numStr, startIndex, endIndex);
+    }
+
+    private static boolean isPalindromeByRecursion2Helper(String numStr, int startIndex, int endIndex){
+        if(startIndex >= endIndex)
+            return true;
+        if(numStr.charAt(startIndex) != numStr.charAt(endIndex))
+            return false;
+
+        return isPalindromeByRecursion2Helper(numStr, ++startIndex, --endIndex);
+    }
 
 
 }
