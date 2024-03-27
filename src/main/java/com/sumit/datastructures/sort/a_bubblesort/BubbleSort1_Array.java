09b9f1397b815a47e74f2ed745900d6e6d007192
Sumit Mittal <sumitmittal.adonis@gmail.com>
2024-03-27 15:37:19 -0400

    Code reorganized

--- src/main/java/com/sumit/datastructures/sort/a_bubblesort/BubbleSort1_Array.java
+++ src/main/java/com/sumit/datastructures/sort/a_bubblesort/BubbleSort1_Array.java
@@ -4,41 +4,38 @@ import java.util.Arrays;
 
 public class BubbleSort1_Array {
 
-/**
-    NOTE : We also did bubble sort using recursion
-           Please check recursion -> sorting -> bubble sort class
-*/
-
     public static void main(String[] args) {
         int[] arr1 = {10, 30, 20, 50, 40};
-        bubbleSort(arr1);
+        bubbleSortWithRecursion(arr1);
         System.out.println(Arrays.toString(arr1));
 
         int[] arr2 = {10, 20, 30, 40, 50};
-        bubbleSort(arr2);
+        bubbleSortWithRecursion(arr2);
         System.out.println(Arrays.toString(arr2));
 
         int[] arr3 = {50, 40, 30, 20, 10};
-        bubbleSort(arr3);
+        bubbleSortWithRecursion(arr3);
         System.out.println(Arrays.toString(arr3));
 
         int[] arr4 = {};
-        bubbleSort(arr4);
+        bubbleSortWithRecursion(arr4);
         System.out.println(Arrays.toString(arr4));
 
         int[] arr5 = {10};
-        bubbleSort(arr5);
+        bubbleSortWithRecursion(arr5);
         System.out.println(Arrays.toString(arr5));
 
         int[] arr6 = {-50, -440, 332, -990, 0};
-        bubbleSort(arr6);
+        bubbleSortWithRecursion(arr6);
         System.out.println(Arrays.toString(arr6));
 
     }
 
+
+    // Way-1 : using for loop
     // we are not returning the existing array because we will sort the existing array
     // because bubble sort comes under inplace sorting technique.
-    private static void bubbleSort(int[] arr) {
+    public static void bubbleSort(int[] arr) {
 
         // iterate the entire array n-1 times
         for(int i = 0; i < arr.length; i++) {
@@ -58,6 +55,38 @@ public class BubbleSort1_Array {
             if(!isSwapped)
                 break;
         }
+    }
+
+
+    // Way-2 : using recursion
+    public static void bubbleSortWithRecursion(int[] arr) {
+        if(arr == null || arr.length == 0)
+            return;
+
+        int i=0;
+        int j=1;
+        helper(arr, i, j);
+    }
+
+    private static void helper(int[] arr, int i, int j) {
+        if(i == arr.length)
+            return;
+
+        // next iteration
+        if(j == arr.length)
+            helper(arr, i+1, 1);
+        else {
+            if(arr[j-1] > arr[j])
+                swap(arr,j-1, j);
+
+            helper(arr, i, j+1);
+        }
+    }
+
+    private static void swap(int[] arr, int a, int b) {
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
     }
 
 }
