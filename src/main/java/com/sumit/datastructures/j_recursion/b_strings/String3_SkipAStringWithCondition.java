bd21534490c5b4d45ce4b5e7610ffd91033ae12f
Sumit Mittal <sumitmittal.adonis@gmail.com>
2024-03-28 10:48:35 -0400

    Recursion : skip app from a string when it is not apple

---
+++ src/main/java/com/sumit/datastructures/j_recursion/b_strings/String3_SkipAStringWithCondition.java
@@ -0,0 +1,46 @@
+package com.sumit.datastructures.j_recursion.b_strings;
+
+public class String3_SkipAStringWithCondition {
+
+    // Program : remove 'app' from the input string if the matching string is not apple
+    // Input : baccappled       => Output : baccappled
+    // Input : baccappld        => Output : baccld
+    public static void main(String[] args) {
+
+
+        // Way-1 : keeping the result in return response
+        System.out.println("baccappled => " + removeA("baccappled"));
+        System.out.println("baccappld  => " + removeA("baccappld"));
+
+        // Way-2 : storing result in an argument
+        removeA2("baccappled", "");
+        removeA2("baccappld", "");
+    }
+
+
+    // Way-1 : returning the result
+    public static String removeA(String input) {
+        if (input == null || input.length() == 0)
+            return "";
+
+        if (input.startsWith("app") && !input.startsWith("apple"))
+            return removeA(input.substring(3));
+        else
+            return input.charAt(0) + removeA(input.substring(1));
+    }
+
+
+    // Way-2 : storing result in an argument
+    public static void removeA2(String input, String answer){
+        if(input == null || input.length() == 0){
+            System.out.println("Ans => " + answer);
+            return;
+        }
+
+        if (input.startsWith("app") && !input.startsWith("apple"))
+            removeA2(input.substring(3), answer);
+        else
+            removeA2(input.substring(1), answer + input.charAt(0));
+    }
+
+}
