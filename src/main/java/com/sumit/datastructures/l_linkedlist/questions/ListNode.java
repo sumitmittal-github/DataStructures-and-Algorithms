c37d73c043373b2c00d276b6a30ebeeeec2ce33d
Sumit Mittal <sumitmittal.adonis@gmail.com>
2024-04-10 20:57:53 -0400

    LinkedList Question : Merge 2 sorted linked list

---
+++ src/main/java/com/sumit/datastructures/l_linkedlist/questions/ListNode.java
@@ -0,0 +1,16 @@
+package com.sumit.datastructures.l_linkedlist.questions;
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+    ListNode(int val) {
+        this.val = val;
+    }
+    ListNode(int val, ListNode next) {
+        this.val = val; this.next = next;
+    }
+
+}
--- src/main/java/com/sumit/datastructures/l_linkedlist/questions/Question2_RemoveDuplicates.java
+++ src/main/java/com/sumit/datastructures/l_linkedlist/questions/Question2_RemoveDuplicates.java
@@ -80,17 +80,3 @@ public class Question2_RemoveDuplicates {
 
 
 }
-
-class ListNode {
-    int val;
-    ListNode next;
-
-    ListNode() {
-    }
-    ListNode(int val) {
-        this.val = val;
-    }
-    ListNode(int val, ListNode next) {
-        this.val = val; this.next = next;
-    }
-}
