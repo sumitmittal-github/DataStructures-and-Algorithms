2fe9e3e4d04937b7c651969d24df1af339c34115
Sumit Mittal <sumitmittal.adonis@gmail.com>
2024-04-13 15:14:39 -0400

    LinkedList : Leet code question : Happy number

---
+++ src/main/java/com/sumit/datastructures/l_linkedlist/questions/Question7_HappyNumber.java
@@ -0,0 +1,49 @@
+package com.sumit.datastructures.l_linkedlist.questions;
+
+import java.util.List;
+import java.util.LinkedList;
+
+public class Question7_HappyNumber {
+
+    // Leet code : https://leetcode.com/problems/happy-number/description/
+
+    public static void main(String[] args) {
+        System.out.println(new Question7_HappyNumber().isHappy(19));
+    }
+
+
+
+
+
+    public boolean isHappy(int inputNum) {
+        if(inputNum == 1 || getSumOfAllDigitsSquare(inputNum) == 1)
+            return true;
+
+        int fast = inputNum;
+        int slow = inputNum;
+
+        fast = getSumOfAllDigitsSquare(getSumOfAllDigitsSquare(fast));
+        slow = getSumOfAllDigitsSquare(slow);
+        while(fast != slow) {
+            if(slow == 1 || fast == 1)
+                return true;
+            fast = getSumOfAllDigitsSquare(getSumOfAllDigitsSquare(fast));
+            slow = getSumOfAllDigitsSquare(slow);
+        }
+        return false;
+    }
+
+
+
+    // Ex : for 25  =  2*2 + 5*5  =  4 + 25 = 29
+    public int getSumOfAllDigitsSquare(int inputNum){
+        int sum = 0;
+        while(inputNum > 0) {
+            int digit = inputNum % 10;
+            sum = sum + (digit * digit);
+            inputNum = inputNum/10;
+        }
+        return sum;
+    }
+
+}
