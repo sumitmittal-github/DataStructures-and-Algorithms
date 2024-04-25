619eda46968660e189c6b4141e8f3dd9f4b20c11
Sumit Mittal <sumitmittal.adonis@gmail.com>
2024-04-25 15:49:51 -0400

    Stack & Queue : Minimum Parentheses

---
+++ src/main/java/com/sumit/datastructures/m_stack_queue/questions/SQ_Q5_MinimumParentheses.java
@@ -0,0 +1,27 @@
+package com.sumit.datastructures.m_stack_queue.questions;
+
+import java.util.Stack;
+
+public class SQ_Q5_MinimumParentheses {
+
+    //LeetCode-921 : https://leetcode.com/problems/minimum-add-to-make-parentheses-valid/description/
+
+    public int minAddToMakeValid(String s) {
+        int count = 0;
+
+        Stack<Character> stack = new Stack<>();
+        for(char c : s.toCharArray()) {
+            if(c == '(')
+                stack.push(c);
+            else {
+                if(!stack.isEmpty())
+                    stack.pop();
+                else
+                    count++;
+            }
+        }
+        return count + stack.size();
+    }
+
+
+}
