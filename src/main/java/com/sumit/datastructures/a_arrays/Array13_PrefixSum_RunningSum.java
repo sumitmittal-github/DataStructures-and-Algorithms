6026ea02fea249d751f475ef3b4e560f29a34af8
Sumit Mittal <sumitmittal.adonis@gmail.com>
2024-04-26 13:48:01 -0400

    College Wallah questions : Arrays : Prefix sum or Running sum

---
+++ src/main/java/com/sumit/datastructures/a_arrays/Array13_PrefixSum_RunningSum.java
@@ -0,0 +1,17 @@
+package com.sumit.datastructures.a_arrays;
+
+public class Array13_PrefixSum_RunningSum {
+
+    //LeetCode-1480 : https://leetcode.com/problems/running-sum-of-1d-array/description/
+
+    public int[] runningSum(int[] arr) {
+        if(arr.length <= 1)
+            return arr;
+
+        for(int i=1; i<arr.length; i++){
+            arr[i] = arr[i] + arr[i-1];
+        }
+        return arr;
+    }
+
+}
