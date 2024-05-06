5ad253a4c10092563a3983d79ed13eb92ff5b93a
Sumit Mittal <sumitmittal.adonis@gmail.com>
2024-05-06 18:23:43 -0400

    Recursion : Integer array subsets sum

--- src/main/java/com/sumit/Test.java
+++ src/main/java/com/sumit/Test.java
@@ -1,9 +1,28 @@
 package com.sumit;
 
+import java.util.*;
+
 public class Test {
 
     public static void main(String[] args) {
-
+        Test t = new Test();
+        System.out.println(t.getSSQHelper("abc", ""));
     }
 
+
+
+
+
+    public ArrayList<String> getSSQHelper(String str, String ans){
+        ArrayList<String> list = new ArrayList<>();
+        if(str == null || str.length() == 0){
+            list.add(ans);
+            return list;
+        }
+
+        list.addAll(getSSQHelper(str.substring(1), ans+str.charAt(0)));
+        list.addAll(getSSQHelper(str.substring(1), ans));
+
+        return list;
+    }
 }
