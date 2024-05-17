49414d83ca7f3f76df2f4aa307d2634fa3d5ccf9
Sumit Mittal <sumitmittal.adonis@gmail.com>
2024-05-16 20:43:25 -0400

    Binary search : Square root upto given precision

--- src/main/java/com/sumit/Test.java
+++ src/main/java/com/sumit/Test.java
@@ -140,32 +140,30 @@ public class Test {
 
 
 
-    // first occurrence
-    public int firstOccurrence(int[] arr, int target){
-        int start=0, end = arr.length-1;
+    public int squareRoot(int num){
+        int start = 1, end = num/2;
 
         while(start <= end){
             int mid = start + (end-start)/2;
 
-            if(arr[mid] == target){
-                if(mid == 0 || arr[mid-1] != target)
-                    return mid;
-                else
-                    end = mid-1;
-            } else if(arr[mid] < target){
+            if(mid*mid == num)
+                return mid;
+            else if(mid*mid < num)
                 start = mid+1;
-            } else
+            else
                 end = mid-1;
         }
-        return -1;
+        return end;
     }
 
 
+    public static void main(String[] args) {
+        double start2 = 4;
+        double end2 = start2 + 1 - Math.pow(.1, 3);
 
-
-
-
-
+        System.out.println(start2);
+        System.out.println(end2);
+    }
 
 
 
