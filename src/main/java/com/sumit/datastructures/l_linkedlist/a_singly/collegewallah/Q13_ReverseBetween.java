a5141cc04dc9eb1d8a4dcac641c9b117eda2fdf0
Sumit Mittal <sumitmittal.adonis@gmail.com>
2024-06-14 17:36:54 -0400

    Linked List : Reverse between

--- src/main/java/com/sumit/datastructures/l_linkedlist/a_singly/collegewallah/Q13_ReverseBetween.java
+++ src/main/java/com/sumit/datastructures/l_linkedlist/a_singly/collegewallah/Q13_ReverseBetween.java
@@ -1,36 +1,31 @@
-package com.sumit.datastructures.l_linkedlist.questions.kunal;
+package com.sumit.datastructures.l_linkedlist.a_singly.collegewallah;
 
-public class LL_Q21_ReverseBetween {
+import com.sumit.datastructures.l_linkedlist.a_singly.ListNode;
+import com.sumit.utils.DSAUtils;
+
+public class Q13_ReverseBetween {
 
     // LeetCode-92 : https://leetcode.com/problems/reverse-linked-list-ii/description/
 
     public ListNode reverseBetween(ListNode head, int left, int right) {
-        if(head == null || head.next == null)
+        // if left == right, then it means no change in the list
+        if(head == null || head.next == null || left == right)
             return head;
-
-        // if left == right, then it means that only 1 need need to be reversed. It means no change in the list
-        if(left == right)
-            return head;
-
 
 
         //STEP-1: get the previous node of left node (from where we will start reversing the list)
         ListNode previousNodeOfLeft = null;
         ListNode nodeOfLeft = head;
         for(int i=1; i<left; i++) {
-            if(previousNodeOfLeft == null) {
-                previousNodeOfLeft = head;
-            } else{
-                previousNodeOfLeft = previousNodeOfLeft.next;
-            }
             nodeOfLeft = nodeOfLeft.next;
+            if(previousNodeOfLeft == null)    previousNodeOfLeft = head;
+            else                              previousNodeOfLeft = previousNodeOfLeft.next;
         }
 
 
-
         // STEP-2 : iterate till right index of linked list and reverse it
-        ListNode previous = previousNodeOfLeft;
         ListNode current = nodeOfLeft;
+        ListNode previous = previousNodeOfLeft;
         while(left <= right){
             ListNode next = current.next;
             current.next = previous;
@@ -45,7 +40,6 @@ public class LL_Q21_ReverseBetween {
         ListNode nextNodeOfRight = current;
 
 
-
         // STEP-3 : attach node at index(left) to node(right+1)
         //        : attach node at index(left-1) to node(right)
         nodeOfLeft.next = nextNodeOfRight;
@@ -57,15 +51,6 @@ public class LL_Q21_ReverseBetween {
         return head;
     }
 
-    public static void display(ListNode head){
-        ListNode temp = head;
-        while(temp != null){
-            System.out.print(temp.val + " -> ");
-            temp = temp.next;
-        }
-        System.out.println("null");
-    }
-
 
     public static void main(String[] args) {
         ListNode head = new ListNode(1,
@@ -74,7 +59,7 @@ public class LL_Q21_ReverseBetween {
                                 new ListNode(4,
                                         new ListNode(5, null)))));
 
-        LL_Q21_ReverseBetween obj = new LL_Q21_ReverseBetween();
-        display(obj.reverseBetween(head, 2, 4));
+        Q13_ReverseBetween obj = new Q13_ReverseBetween();
+        DSAUtils.displayLinkedList(obj.reverseBetween(head, 2, 4));
     }
 }
