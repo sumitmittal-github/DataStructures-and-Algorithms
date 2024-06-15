90b4c6535429d6d0aa1105dc529241d2794a3674
Sumit Mittal <sumitmittal.adonis@gmail.com>
2024-06-15 10:09:25 -0400

    Linked List : Palindrome

--- src/main/java/com/sumit/datastructures/l_linkedlist/a_singly/collegewallah/Q15_Palindrome.java
+++ src/main/java/com/sumit/datastructures/l_linkedlist/a_singly/collegewallah/Q15_Palindrome.java
@@ -6,7 +6,46 @@ public class Q15_Palindrome {
 
     // LeetCode-234 : https://leetcode.com/problems/palindrome-linked-list/description/
 
-    public boolean isPalindrome(ListNode head) {
+
+    // Approach-1 : finding the left mid node in .5 iteration, using fast and slow pointer
+    public boolean isPalindrome_1(ListNode head) {
+
+        // Step-1 : find the leftMid
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        ListNode leftMid = slow;
+
+        // Step-2 : Reverse the 2nd half linkedlist
+        ListNode previous = null;
+        ListNode current = leftMid.next;
+        while(current != null){
+            ListNode next = current.next;
+            current.next = previous;
+            previous = current;
+            current = next;
+        }
+        leftMid.next = previous;
+
+        // Step-3 : compare values of both halfs
+        ListNode first = head;
+        ListNode second = leftMid.next;
+        while(second != null) {
+            if(first.val != second.val)
+                return false;
+            first = first.next;
+            second = second.next;
+        }
+        return true;
+    }
+
+
+
+    // Approach-2 : finding the left mid node in 1.5 iteration
+    public boolean isPalindrome_2(ListNode head) {
 
         // Step-1 : find the size of linked list
         int size = 0;
