ce3a6ac05b25efd53f92657bd94e750afb447363
Sumit Mittal <sumitmittal.adonis@gmail.com>
2024-06-15 15:27:02 -0400

    Doubly Linked List : Palindrome

--- src/main/java/com/sumit/datastructures/l_linkedlist/b_doubly/collegewallah/Q1_Palindrome.java
+++ src/main/java/com/sumit/datastructures/l_linkedlist/b_doubly/collegewallah/Q1_Palindrome.java
@@ -6,10 +6,7 @@ import com.sumit.datastructures.l_linkedlist.b_doubly.ListNodeD;
 public class Q1_Palindrome {
 
     // Question : check if a given doubly linked list is palindrome
-    public boolean isPalindrome(DoublyLinkedList list){
-        if(list == null)            return false;
-
-        ListNodeD head = list.get(0);
+    public boolean isPalindrome(ListNodeD head){
         if(head == null)            return false;
         if(head.next == null)       return true;
 
@@ -34,28 +31,28 @@ public class Q1_Palindrome {
     public static void main(String[] args) {
         Q1_Palindrome obj = new Q1_Palindrome();
 
-        DoublyLinkedList head1 = new DoublyLinkedList();
-        head1.insertAtTail(1);     head1.insertAtTail(2);     head1.insertAtTail(1);
-        head1.display();
-        System.out.println(obj.isPalindrome(head1));
+        DoublyLinkedList list1 = new DoublyLinkedList();
+        list1.insertAtTail(1);     list1.insertAtTail(2);     list1.insertAtTail(1);
+        list1.display();
+        System.out.println(obj.isPalindrome(list1.get(0)));
         System.out.println();
         
-        DoublyLinkedList head2 = new DoublyLinkedList();
-        head2.insertAtTail(1);     head2.insertAtTail(1);
-        head2.display();
-        System.out.println(obj.isPalindrome(head2));
+        DoublyLinkedList list2 = new DoublyLinkedList();
+        list2.insertAtTail(1);     list2.insertAtTail(1);
+        list2.display();
+        System.out.println(obj.isPalindrome(list2.get(0)));
         System.out.println();
         
-        DoublyLinkedList head3 = new DoublyLinkedList();
-        head3.insertAtTail(1);     head3.insertAtTail(2);
-        head3.display();
-        System.out.println(obj.isPalindrome(head3));
+        DoublyLinkedList list3 = new DoublyLinkedList();
+        list3.insertAtTail(1);     list3.insertAtTail(2);
+        list3.display();
+        System.out.println(obj.isPalindrome(list3.get(0)));
         System.out.println();
         
-        DoublyLinkedList head4 = new DoublyLinkedList();
-        head4.insertAtTail(1);     head4.insertAtTail(2);     head4.insertAtTail(3);
-        head4.display();
-        System.out.println(obj.isPalindrome(head4));
+        DoublyLinkedList list4 = new DoublyLinkedList();
+        list4.insertAtTail(1);     list4.insertAtTail(2);     list4.insertAtTail(3);
+        list4.display();
+        System.out.println(obj.isPalindrome(list4.get(0)));
     }
 
 }
